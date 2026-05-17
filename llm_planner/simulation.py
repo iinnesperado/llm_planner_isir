25c6696caf265139f4d8f805c0c4d01fe092e2f4
iinnesperado <inestianrp@gmail.com>
2026-05-17 15:38:35 +0200

    Add Franka simulation configuration and implement basic simulator functionality tests

--- llm_planner/simulation.py
+++ llm_planner/simulation.py
@@ -1,112 +1,210 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.callback_groups import ReentrantCallbackGroup
 from rcl_interfaces.msg import ParameterDescriptor
-from core.service_client import ServiceClient
-
-from core_interfaces.srv import LoadConfig
 from core.utils import class_from_classname
+import yaml
+import copy
+
+from cognitive_node_interfaces.msg import PerceptionStamped
+from std_msgs.msg import String
+from core_interfaces.srv import LoadConfig
+
+
+class SemanticPerceptionConverter:
+    """
+    Converts Franka simulator state to semantic perception format compatible with space.py
+    """
+    def __init__(self):
+        self.location_names = {
+            "base": "robot_base",
+            "table": "table_top",
+            "gripper": "in_gripper"
+        }
+    
+    def state_to_perception_dict(self, arm_state, world_state):
+        """
+        Convert simulator state to semantic perception dict.
+        
+        Expected output format (from space.py):
+        {
+            'location': str,           # robot location
+            'grasped_obj': dict,       # what's in gripper {obj_id: part}
+            'known_obj': dict,         # all known objects {obj_id: location}
+            'observed_obj': dict,      # currently visible objects
+        }
+        """
+        grasped_obj = {}
+        if arm_state['grasped_object']:
+            grasped_obj[arm_state['grasped_object']] = arm_state.get('grasped_part', 'body')
+        
+        perception_dict = {
+            'location': arm_state.get('location', 'base'),
+            'grasped_obj': grasped_obj,
+            'known_obj': world_state.get('objects', {}),
+            'observed_obj': world_state.get('visible_objects', {}),
+        }
+        return perception_dict
+
+
+class FrankaSimulator:
+    """
+    Discrete event simulator for Franka robotic arm with semantic perception.
+    """
+    def __init__(self):
+        # Franka state
+        self.joint_angles = [0.0] * 7  # 7-DOF arm
+        self.gripper_width = 0.04      # Open (meters)
+        self.gripper_force = 0.0
+        self.location = "base"         # Current location
+        
+        # Object tracking
+        self.grasped_object = None     # Currently held object
+        self.grasped_part = "body"     # Which part of object is grasped
+        self.world_objects = {}        # {obj_id: {location, type, subparts}}
+        self.visible_objects = {}      # Objects in current FOV
+        
+    def grasp_object(self, obj_id, subpart="body"):
+        """Grasp an object if it's visible"""
+        if obj_id in self.visible_objects:
+            self.grasped_object = obj_id
+            self.grasped_part = subpart
+            self.gripper_width = 0.0
+            self.visible_objects.pop(obj_id)  # Remove from visible
+            return True
+        return False
+    
+    def place_object(self, location):
+        """Place grasped object at location if gripper holds something"""
+        if self.grasped_object:
+            self.world_objects[self.grasped_object]['location'] = location
+            self.visible_objects[self.grasped_object] = self.world_objects[self.grasped_object]
+            self.grasped_object = None
+            self.grasped_part = "body"
+            self.gripper_width = 0.04
+            return True
+        return False
+    
+    def move_to_location(self, location):
+        """Move to a new location"""
+        if location in self.world_objects or location == "base":
+            self.location = location
+            self.update_visible_objects()
+            return True
+        return False
+    
+    def update_visible_objects(self):
+        """Update which objects are visible at current location"""
+        self.visible_objects = {}
+        for obj_id, obj_data in self.world_objects.items():
+            if obj_data.get('location') == self.location and obj_id != self.grasped_object:
+                self.visible_objects[obj_id] = obj_data
+    
+    def get_state(self):
+        """Get current arm state"""
+        return {
+            'joint_angles': copy.copy(self.joint_angles),
+            'gripper_width': self.gripper_width,
+            'location': self.location,
+            'grasped_object': self.grasped_object,
+            'grasped_part': self.grasped_part,
+        }
+    
+    def set_world_objects(self, objects_dict):
+        """Initialize world objects: {obj_id: {location, type, subparts}}"""
+        self.world_objects = copy.deepcopy(objects_dict)
+        self.update_visible_objects()
+
 
 class LLMSim(Node):
     def __init__(self):
         super().__init__("LLMSimulation")
-        self.perceived_objects = {}  # {obj_id: location}
-        self.grasped_objects = {}    # {obj_id: hand} (could be left or right if there's two hands)
-
-        self.config_file = self.declare_parameter('config_file', descriptor=ParameterDescriptor(dynamic_typing=True)).get_parameter_value().string_value
-
+        
+        # Callback groups for concurrency
+        self.cbgroup_server = ReentrantCallbackGroup()
+        self.cbgroup_perception = ReentrantCallbackGroup()
+        
+        # Simulators
+        self.franka_sim = FrankaSimulator()
+        self.perception_converter = SemanticPerceptionConverter()
+        
+        # Publisher for semantic perception
+        self.perception_pub = self.create_publisher(
+            PerceptionStamped,
+            "perception/llm_planner/value",
+            1,
+            callback_group=self.cbgroup_perception
+        )
+        
+        # Configuration
+        self.config_file = self.declare_parameter(
+            'config_file',
+            descriptor=ParameterDescriptor(dynamic_typing=True)
+        ).get_parameter_value().string_value
+        
+        self.load_configuration()
+        self.get_logger().info("LLMSimulation initialized")
     
-    def grasp_object_policy(self, obj_id):
-        """Grasp l'objet s'il est observé"""
-        if obj_id in self.perceived_objects:
-            self.grasped_objects[obj_id] = "hand"
-            del self.perceived_objects[obj_id]
-            self.publish_perceptions()
-            return True
-        return False
-    
-    def place_object_policy(self, obj_id, location):
-        """Place l'objet s'il est saisi"""
-        if obj_id in self.grasped_objects:
-            self.perceived_objects[obj_id] = location
-            del self.grasped_objects[obj_id]
-            self.publish_perceptions()
-            return True
-        return False
-    
-    def reward_pick(self):
-
-        return 
-    
-    def reward_place(self):
-        return
-    
-    def publish_perceptions(self):
-        """Publier l'état actuel de la perception"""
-        perception = {
-            'observed_obj': self.perceived_objects,
-            'grasped_obj': self.grasped_objects,
-            'location': "robot_location"
-        }
-
-    def new_action_service_callback(self, request, response):
-        """
-        Execute a policy and publish new perceptions.
-
-        :param request: The message that contains the policy to execute.
-        :type request: ROS srv defined in the config file. Typically cognitive_node_interfaces.srv.Policy.Request
-        :param response: Response of the success of the execution of the action.
-        :type response: ROS srv defined in the config file. Typically cognitive_node_interfaces.srv.Policy.Response
-        :return: Response indicating the success of the action execution.
-        :rtype: ROS srv defined in the config file. Typically cognitive_node_interfaces.srv.Policy.Response
-        """
-        self.get_logger().info("Executing policy " + str(request.policy))
-        getattr(self, request.policy + "_policy")()
-        self.update_reward_sensor()
-        self.publish_perceptions()
-        if (not self.catched_object) and (
-            self.perceptions["ball_in_left_hand"].data
-            or self.perceptions["ball_in_right_hand"].data
-        ):
-            self.get_logger().error("Critical error: catched_object is empty and it should not!!!")
-            rclpy.shutdown()
-        response.success = True
-        return response
-
-    def setup_control_channel(self, simulation):
-        """
-        # NOTE Copy pasted, so adapt for sim
-        Configure the ROS topic/service where listen for commands to be executed.
-
-        :param simulation: The params from the config file to setup the control channel.
-        :type simulation: dict
-        """
-        self.ident = simulation["id"]
-        topic = simulation["control_topic"]
-        classname = simulation["control_msg"]
-        message = class_from_classname(classname)
-        self.get_logger().info("Subscribing to... " + str(topic))
-        self.create_subscription(message, topic, self.new_command_callback, 0)
-        topic = simulation.get("executed_policy_topic")
-        service_policy = simulation.get("executed_policy_service")
-        service_world_reset = simulation.get("world_reset_service")
-
-        if topic:
-            self.get_logger().info("Subscribing to... " + str(topic))
-            self.create_subscription(message, topic, self.new_action_callback, 0)
-        if service_policy:
-            self.get_logger().info("Creating server... " + str(service_policy))
-            classname = simulation["executed_policy_msg"]
-            message_policy_srv = class_from_classname(classname)
-            self.create_service(message_policy_srv, service_policy, self.new_action_service_callback, callback_group=self.cbgroup_server)
-            self.get_logger().info("Creating perception publisher timer... ")
-            self.perceptions_timer = self.create_timer(0.01, self.publish_perceptions, callback_group=self.cbgroup_server)
-        if service_world_reset:
-            classname= simulation["executed_policy_msg"]
-            self.message_world_reset = class_from_classname(simulation["world_reset_msg"])
-            self.create_service(self.message_world_reset, service_world_reset, self.world_reset_service_callback, callback_group=self.cbgroup_server)     
-
     def load_configuration(self):
         """
-        Load configuration from a file.
+        Load simulation configuration from YAML file.
+        Expected format:
+        world_objects:
+          mug: {location: 'table', type: 'cup', subparts: ['body', 'handle']}
+          plate: {location: 'table', type: 'container', subparts: ['body']}
+        initial_location: 'base'
         """
-        return 
+        if self.config_file:
+            try:
+                with open(self.config_file, 'r') as f:
+                    config = yaml.safe_load(f)
+                
+                # Load world objects
+                if 'world_objects' in config:
+                    self.franka_sim.set_world_objects(config['world_objects'])
+                    self.get_logger().info(f"Loaded objects: {list(config['world_objects'].keys())}")
+                
+                # Set initial location
+                if 'initial_location' in config:
+                    self.franka_sim.location = config['initial_location']
+                    self.franka_sim.update_visible_objects()
+                
+            except Exception as e:
+                self.get_logger().warn(f"Could not load config file: {e}")
+    
+    def grasp_object_policy(self, obj_id, subpart="body"):
+        """Grasp an object if it's visible at current location"""
+        success = self.franka_sim.grasp_object(obj_id, subpart)
+        self.publish_perceptions()
+        return success
+    
+    def place_object_policy(self, location):
+        """Place currently grasped object at location"""
+        success = self.franka_sim.place_object(location)
+        self.publish_perceptions()
+        return success
+    
+    def move_to_location_policy(self, location):
+        """Move to a new location"""
+        success = self.franka_sim.move_to_location(location)
+        self.publish_perceptions()
+        return success
+    
+    def publish_perceptions(self):
+        """Publish current semantic perception as PerceptionStamped message"""
+        arm_state = self.franka_sim.get_state()
+        world_state = {
+            'objects': self.franka_sim.world_objects,
+            'visible_objects': self.franka_sim.visible_objects,
+        }
+        
+        perception_dict = self.perception_converter.state_to_perception_dict(arm_state, world_state)
+        
+        # Create PerceptionStamped message
+        msg = PerceptionStamped()
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.perception.data = yaml.dump(perception_dict)
+        
+        self.perception_pub.publish(msg)
+        self.get_logger().debug(f"Published perception: {perception_dict}")
+ 
---
+++ llm_planner/test_simulation.py
@@ -0,0 +1,112 @@
+"""
+Simple test script to demonstrate FrankaSimulator and SemanticPerceptionConverter
+without needing ROS running. Use this to test before integrating with ROS node.
+"""
+
+from simulation import FrankaSimulator, SemanticPerceptionConverter
+import yaml
+
+
+def test_franka_simulator():
+    """Test basic simulator functionality"""
+    print("=" * 60)
+    print("Testing Franka Simulator")
+    print("=" * 60)
+    
+    # Initialize simulator
+    sim = FrankaSimulator()
+    converter = SemanticPerceptionConverter()
+    
+    # Load world objects
+    world_objects = {
+        'mug': {
+            'location': 'table',
+            'type': 'cup',
+            'subparts': ['body', 'handle']
+        },
+        'plate': {
+            'location': 'table',
+            'type': 'container',
+            'subparts': ['body']
+        },
+        'bottle': {
+            'location': 'shelf',
+            'type': 'container',
+            'subparts': ['body', 'cap']
+        }
+    }
+    
+    sim.set_world_objects(world_objects)
+    print(f"\n✓ Loaded objects: {list(world_objects.keys())}")
+    
+    # Initial state
+    print("\n--- Initial State ---")
+    state = sim.get_state()
+    perception = converter.state_to_perception_dict(state, {
+        'objects': sim.world_objects,
+        'visible_objects': sim.visible_objects,
+    })
+    print(f"Location: {perception['location']}")
+    print(f"Visible objects: {list(perception['observed_obj'].keys())}")
+    print(f"Grasped objects: {perception['grasped_obj']}")
+    
+    # Test 1: Grasp object
+    print("\n--- Test 1: Grasp mug by handle ---")
+    success = sim.grasp_object('mug', 'handle')
+    print(f"✓ Grasped: {success}")
+    state = sim.get_state()
+    perception = converter.state_to_perception_dict(state, {
+        'objects': sim.world_objects,
+        'visible_objects': sim.visible_objects,
+    })
+    print(f"Grasped objects: {perception['grasped_obj']}")
+    print(f"Visible objects: {list(perception['observed_obj'].keys())}")
+    
+    # Test 2: Move to location
+    print("\n--- Test 2: Move to shelf ---")
+    success = sim.move_to_location('shelf')
+    print(f"✓ Moved: {success}")
+    state = sim.get_state()
+    perception = converter.state_to_perception_dict(state, {
+        'objects': sim.world_objects,
+        'visible_objects': sim.visible_objects,
+    })
+    print(f"Location: {perception['location']}")
+    print(f"Visible objects at shelf: {list(perception['observed_obj'].keys())}")
+    
+    # Test 3: Place object
+    print("\n--- Test 3: Place mug on shelf ---")
+    success = sim.place_object('shelf')
+    print(f"✓ Placed: {success}")
+    state = sim.get_state()
+    perception = converter.state_to_perception_dict(state, {
+        'objects': sim.world_objects,
+        'visible_objects': sim.visible_objects,
+    })
+    print(f"Grasped objects: {perception['grasped_obj']}")
+    print(f"Visible objects: {list(perception['observed_obj'].keys())}")
+    print(f"Mug location: {sim.world_objects['mug']['location']}")
+    
+    # Test 4: Move back and grasp bottle
+    print("\n--- Test 4: Move back to table and grasp bottle ---")
+    sim.move_to_location('table')
+    success = sim.grasp_object('bottle', 'cap')
+    print(f"✓ Grasped bottle by cap: {success}")
+    state = sim.get_state()
+    perception = converter.state_to_perception_dict(state, {
+        'objects': sim.world_objects,
+        'visible_objects': sim.visible_objects,
+    })
+    print(f"Grasped: {perception['grasped_obj']}")
+    
+    print("\n" + "=" * 60)
+    print("✓ All tests passed!")
+    print("=" * 60)
+    
+    # Show full perception dict
+    print("\nFinal Perception Dict (YAML format):")
+    print(yaml.dump(perception, default_flow_style=False))
+
+
+if __name__ == "__main__":
+    test_franka_simulator()
